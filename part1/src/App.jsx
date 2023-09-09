0a9ece28640d73e332ac1c180e188d86d0551a7e
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-09 17:10:53 +0200

    Part 1 - C

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,72 +1,35 @@
-const App = () => {
+import { useState } from 'react'
 
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        name: "State of a component",
-        exercises: 14
-      }
-    ]
-  }
+
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
-
-
-
-/*
-  Components
-*/
-
-const Header = (props) => {
-  return (
-    <h1>{props.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return props.parts.map(part => {
-    return (
-      <Part name={part.name} count={part.exercises} />
-    )
-  })
-}
-
-const Part = (part) => {
-  return (
-    <p>
-      {part.name} {part.count}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  const exercicesCount = props.parts.reduce(function (sum, part) {
-    return sum + part.exercises;
-  }, 0)
-
-  return (
-    <p>
-      Number of exercises {exercicesCount}
-    </p>
-  )
-}
-
-
 
 export default App
