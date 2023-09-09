31ba20b2655a57369b42c6174ebf35bd6e674814
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-09 22:37:34 +0200

    Part 1 - Exercise 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,72 +1,28 @@
 import { useState } from 'react'
 
-
-const Button = ({ name, handler }) => {
-  return (
-    <button onClick={handler}>{name}</button>
-  )
-}
-
-const StatisticLine = ({ name, value }) => {
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ good, neutral, bad }) => {
-
-  const sum = calcSum(good, neutral, bad)
-  const avgWeighted = calcAvgWeighted(good, bad, sum)
-  const positivePercent = calcPositivePercent(good, sum)
-
-  if (sum !== 0) {
-    return (
-      <div>
-        <table>
-          <tbody>
-            <StatisticLine name="good" value={good} />
-            <StatisticLine name="neutral" value={neutral} />
-            <StatisticLine name="bad" value={bad} />
-            <StatisticLine name="sum" value={sum} />
-            <StatisticLine name="avgWeighted" value={avgWeighted} />
-            <StatisticLine name="positivePercent" value={positivePercent} />
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-  else {
-    return (
-      <p>No feedback given</p>
-    )
-  }
-}
-
-// const sum = (values) => values.reduce((acc, i) => acc + i, 0)
-const calcSum = (...args) => args.reduce((acc, i) => acc + i, 0)
-const calcAvgWeighted = (good, bad, sum) => (good - bad) / sum
-const calcPositivePercent = (good, sum) => good / sum * 100
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button name="good" handler={() => setGood(good + 1)} />
-      <Button name="neutral" handler={() => setNeutral(neutral + 1)} />
-      <Button name="bad" handler={() => setBad(bad + 1)} />
-
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-
-    </div>
+    <>
+      <div>
+        {anecdotes[selected]}
+      </div>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+        Next anecdote
+      </button>
+    </>
   )
 }
 
