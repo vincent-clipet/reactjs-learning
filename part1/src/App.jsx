b126a3a8089b9eccb5ded8bad2540c77e0d7b966
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-09 18:14:46 +0200

    Part 1 - Exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,54 +1,38 @@
 import { useState } from 'react'
 
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
+const Button = ({name, handler}) => {
   return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+    <button onClick={handler}>{name}</button>
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const Statistic = ({name, value}) => {
+  return (
+    <p>{name} {value}</p>
+  )
+}
+
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <h1>give feedback</h1>
+      <Button name="good"     handler={() => setGood(good + 1)} />
+      <Button name="neutral"  handler={() => setNeutral(neutral + 1)} />
+      <Button name="bad"      handler={() => setBad(bad + 1)} />
+
+      <h1>statistics</h1>
+      <Statistic name="good"    value={good} />
+      <Statistic name="neutral" value={neutral} />
+      <Statistic name="bad"     value={bad} />
     </div>
   )
 }
-
-
 
 export default App
