81e2474064823d1f8f2800feb7b6852aa6856641
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-09 22:18:05 +0200

    Part 1 - Exercise 1.10

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,66 +1,66 @@
 import { useState } from 'react'
 
 
-const Button = ({name, handler}) => {
+const Button = ({ name, handler }) => {
   return (
     <button onClick={handler}>{name}</button>
+  )
+}
+
+const StatisticLine = ({ name, value }) => {
+  return (
+    <p>{name} {value}</p>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+
+  const sum = calcSum(good, neutral, bad)
+  const avgWeighted = calcAvgWeighted(good, bad, sum)
+  const positivePercent = calcPositivePercent(good, sum)
+
+  if (sum !== 0) {
+    return (
+      <div>
+        <StatisticLine name="good" value={good} />
+        <StatisticLine name="neutral" value={neutral} />
+        <StatisticLine name="bad" value={bad} />
+        <StatisticLine name="sum" value={sum} />
+        <StatisticLine name="avgWeighted" value={avgWeighted} />
+        <StatisticLine name="positivePercent" value={positivePercent} />
+      </div>
     )
   }
-  
-  const Statistic = ({name, value}) => {
+  else {
     return (
-      <p>{name} {value}</p>
-      )
-    }
-    
-    const Statistics = ({good, neutral, bad}) => {
-      
-      const sum = calcSum(good, neutral, bad)
-      const avgWeighted = calcAvgWeighted(good, bad, sum)
-      const positivePercent = calcPositivePercent(good, sum)
-      
-      if (sum !== 0) {
-        return (
-          <div>
-          <Statistic name="good"    value={good} />
-          <Statistic name="neutral" value={neutral} />
-          <Statistic name="bad"     value={bad} />
-          <Statistic name="sum"               value={sum} />
-          <Statistic name="avgWeighted"       value={avgWeighted} />
-          <Statistic name="positivePercent"   value={positivePercent} />
-          </div>
-          )
-        }
-        else {
-          return (
-            <p>No feedback given</p>
-          )
-        }
-      }
-      
-      // const sum = (values) => values.reduce((acc, i) => acc + i, 0)
-      const calcSum = (...args) => args.reduce((acc, i) => acc + i, 0)
-      const calcAvgWeighted = (good, bad, sum) => (good - bad) / sum
-      const calcPositivePercent = (good, sum) => good / sum * 100
-      
-      const App = () => {
-        // save clicks of each button to its own state
-        const [good, setGood] = useState(0)
-        const [neutral, setNeutral] = useState(0)
-        const [bad, setBad] = useState(0)
-        
-        return (
-          <div>
-          <h1>give feedback</h1>
-          <Button name="good"     handler={() => setGood(good + 1)} />
-          <Button name="neutral"  handler={() => setNeutral(neutral + 1)} />
-          <Button name="bad"      handler={() => setBad(bad + 1)} />
-          
-          <h1>statistics</h1>
-          <Statistics good={good} neutral={neutral} bad={bad} />
-          
-          </div>
-          )
-        }
-        
-        export default App
+      <p>No feedback given</p>
+    )
+  }
+}
+
+// const sum = (values) => values.reduce((acc, i) => acc + i, 0)
+const calcSum = (...args) => args.reduce((acc, i) => acc + i, 0)
+const calcAvgWeighted = (good, bad, sum) => (good - bad) / sum
+const calcPositivePercent = (good, sum) => good / sum * 100
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button name="good" handler={() => setGood(good + 1)} />
+      <Button name="neutral" handler={() => setNeutral(neutral + 1)} />
+      <Button name="bad" handler={() => setBad(bad + 1)} />
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+export default App
