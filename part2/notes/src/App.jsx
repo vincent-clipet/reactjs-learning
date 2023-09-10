6d25e64a06ec29ae2459241bc66a084f0c3ac158
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-10 17:03:15 +0200

    Part 2 - C

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,106 +1,61 @@
-import { useState } from 'react'
-
-let idGenerator = 0
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '0123' },
-    { name: 'Azer Ty', phone: '4567' },
-    { name: 'Az Erty', phone: '8901' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [search, setSearch] = useState('')
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
-
-
-
-
-  let personsToShow = persons
-  if (search !== "") {
-    const regexp = new RegExp(search.toLowerCase());
-    personsToShow = persons.filter(person => person.name.toLowerCase().search(regexp) !== -1)
-  }
-
-  const handleNewNameChange = (event) => setNewName(event.target.value)
-  const handleNewPhoneChange = (event) => setNewPhone(event.target.value)
-  const handleSearch = (event) => setSearch(event.target.value)
-  const handleFormSubmit = (event) => {
+  const addNote = (event) => {
     event.preventDefault()
-
-    // This name already exists
-    if (persons.find((element) => element.name == newName)) {
-      alert(`${newName} is already added to phonebook`)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
-    else {
-      const newPerson = { name: newName, phone: newPhone }
-      setPersons(persons.concat(newPerson))
-      setNewName("")
-      setNewPhone("")
-    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
-
-      <h2>Phonebook</h2>
-      <SearchFilter
-        handleSearch={handleSearch}
-        search={search}
-      />
-
-      <h3>Add a new person</h3>
-      <NewPersonForm
-        newName={newName}
-        newPhone={newPhone}
-        handleNewNameChange={handleNewNameChange}
-        handleNewPhoneChange={handleNewPhoneChange}
-        handleFormSubmit={handleFormSubmit}
-      />
-
-      <h3>Numbers</h3>
-      <Persons
-        personsToShow={personsToShow}
-      />
-
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+      <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form> 
     </div>
-  )
-}
-
-const SearchFilter = ({ handleSearch, search }) => {
-  return (
-    <div>filter shown with
-      <input onChange={handleSearch} value={search} />
-    </div>
-  )
-}
-
-const NewPersonForm = ({ newName, newPhone, handleNewNameChange, handleNewPhoneChange, handleFormSubmit }) => {
-  return (
-    <form>
-      <div>name:    <input onChange={handleNewNameChange} value={newName} />        </div>
-      <div>number:  <input onChange={handleNewPhoneChange} value={newPhone} />      </div>
-      <div>         <button type="submit" onClick={handleFormSubmit}>add</button>   </div>
-    </form>
-  )
-}
-
-const Persons = ({ personsToShow }) => {
-  return (
-    <ul>
-      {personsToShow.map(person =>
-        <Person key={idGenerator++} name={person.name} phone={person.phone} />
-      )}
-    </ul>
-  )
-}
-
-const Person = ({ name, phone }) => {
-  return (
-    <li>{name} - {phone}</li>
   )
 }
 
