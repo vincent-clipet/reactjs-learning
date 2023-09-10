fb755466597691588a093903226e4329470ce980
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-10 14:38:47 +0200

    Part 2 - B

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,69 +1,16 @@
-import Course from './components/Course'
+import Note from './components/Note'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  return (
-    <>
-      <h1>Web development curriculum</h1>
-      <Courses courses={courses} />
-    </>
-  )
-}
-
-const Courses = ({ courses }) => {
+const App = ({ notes }) => {
   return (
     <div>
-      {courses.map(course =>
-        <Course key={course.id} course={course} />
-      )}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
-
-
 
 export default App
