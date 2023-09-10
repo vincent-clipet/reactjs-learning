9a4b99f26d123b0fba6eab0662ace0b0d1c382d9
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-10 16:23:18 +0200

    Part 2 - Exercise 2.10

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 
+let idGenerator = 0
+
 const App = () => {
 
-  let idGenerator = 0
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '0123' },
     { name: 'Azer Ty', phone: '4567' },
@@ -14,6 +15,17 @@ const App = () => {
 
 
 
+
+
+  let personsToShow = persons
+  if (search !== "") {
+    const regexp = new RegExp(search.toLowerCase());
+    personsToShow = persons.filter(person => person.name.toLowerCase().search(regexp) !== -1)
+  }
+
+  const handleNewNameChange = (event) => setNewName(event.target.value)
+  const handleNewPhoneChange = (event) => setNewPhone(event.target.value)
+  const handleSearch = (event) => setSearch(event.target.value)
   const handleFormSubmit = (event) => {
     event.preventDefault()
 
@@ -29,41 +41,60 @@ const App = () => {
     }
   }
 
-  let personsToShow = persons
-  if (search !== "") {
-    const regexp = new RegExp(search.toLowerCase());
-    personsToShow = persons.filter(person => person.name.toLowerCase().search(regexp) !== -1)
-  }
-
-  const handleNewNameChange = (event) => setNewName(event.target.value)
-  const handleNewPhoneChange = (event) => setNewPhone(event.target.value)
-  const handleSearch = (event) => setSearch(event.target.value)
-
 
 
   return (
     <div>
 
       <h2>Phonebook</h2>
-      <div>filter shown with
-        <input onChange={handleSearch} value={search} />
-      </div>
+      <SearchFilter
+        handleSearch={handleSearch}
+        search={search}
+      />
 
-      <h2>Add a new</h2>
-      <form>
-        <div>name:    <input onChange={handleNewNameChange} value={newName} />        </div>
-        <div>number:  <input onChange={handleNewPhoneChange} value={newPhone} />      </div>
-        <div>         <button type="submit" onClick={handleFormSubmit}>add</button>   </div>
-      </form>
+      <h3>Add a new person</h3>
+      <NewPersonForm
+        newName={newName}
+        newPhone={newPhone}
+        handleNewNameChange={handleNewNameChange}
+        handleNewPhoneChange={handleNewPhoneChange}
+        handleFormSubmit={handleFormSubmit}
+      />
 
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person =>
-          <Person key={idGenerator++} name={person.name} phone={person.phone} />
-        )}
-      </ul>
+      <h3>Numbers</h3>
+      <Persons
+        personsToShow={personsToShow}
+      />
 
     </div>
+  )
+}
+
+const SearchFilter = ({ handleSearch, search }) => {
+  return (
+    <div>filter shown with
+      <input onChange={handleSearch} value={search} />
+    </div>
+  )
+}
+
+const NewPersonForm = ({ newName, newPhone, handleNewNameChange, handleNewPhoneChange, handleFormSubmit }) => {
+  return (
+    <form>
+      <div>name:    <input onChange={handleNewNameChange} value={newName} />        </div>
+      <div>number:  <input onChange={handleNewPhoneChange} value={newPhone} />      </div>
+      <div>         <button type="submit" onClick={handleFormSubmit}>add</button>   </div>
+    </form>
+  )
+}
+
+const Persons = ({ personsToShow }) => {
+  return (
+    <ul>
+      {personsToShow.map(person =>
+        <Person key={idGenerator++} name={person.name} phone={person.phone} />
+      )}
+    </ul>
   )
 }
 
