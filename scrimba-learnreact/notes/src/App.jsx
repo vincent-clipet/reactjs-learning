b087309fda800179ec4eabfdc9d2f74b96702197
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-16 19:08:52 +0200

    Notes: Firebase setup

--- scrimba-learnreact/notes/src/App.jsx
+++ scrimba-learnreact/notes/src/App.jsx
@@ -1,30 +1,24 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import { nanoid } from "nanoid"
 
-/**
- * Challenge: Spend 10-20+ minutes reading through the code
- * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you 
- * understand the existing code (although you don't need
- * to fully understand everything to move on)
- */
-
 export default function App() {
-
-  const localStorageLoad = () => JSON.parse(localStorage.getItem("notes")) || []
-
-  const [notes, setNotes] = React.useState(localStorageLoad)
-  const [currentNoteId, setCurrentNoteId] = React.useState((notes[0] && notes[0].id) || "")
-
-  const localStorageSave = React.useEffect(
-    () => localStorage.setItem("notes", JSON.stringify(notes)), [notes]
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  )
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0]?.id) || ""
   )
 
+  const currentNote =
+    notes.find(note => note.id === currentNoteId)
+    || notes[0]
 
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
 
   function createNewNote() {
     const newNote = {
@@ -35,41 +29,26 @@ export default function App() {
     setCurrentNoteId(newNote.id)
   }
 
-  // function updateNote(text) {
-  //   setNotes(oldNotes => oldNotes.map(oldNote => {
-  //     return oldNote.id === currentNoteId
-  //       ? { ...oldNote, body: text }
-  //       : oldNote
-  //   }))
-  // }
-
   function updateNote(text) {
-
     setNotes(oldNotes => {
-      const updatedNotes = []
-      oldNotes.map(oldNote => {
-        if (oldNote.id === currentNoteId)
-          updatedNotes.unshift({ ...oldNote, body: text })
-        else
-          updatedNotes.push(oldNote)
-      })
-      return updatedNotes
+      const newArray = []
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i]
+        if (oldNote.id === currentNoteId) {
+          // Put the most recently-modified note at the top
+          newArray.unshift({ ...oldNote, body: text })
+        } else {
+          newArray.push(oldNote)
+        }
+      }
+      return newArray
     })
-  }
-
-  function findCurrentNote() {
-    return notes.find(note => {
-      return note.id === currentNoteId
-    }) || notes[0]
   }
 
   function deleteNote(event, noteId) {
     event.stopPropagation()
-    const updatedNotes = notes.filter(oldNote => oldNote.id !== noteId)
-    setNotes(updatedNotes)
+    setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
   }
-
-
 
   return (
     <main>
@@ -83,7 +62,7 @@ export default function App() {
           >
             <Sidebar
               notes={notes}
-              currentNote={findCurrentNote()}
+              currentNote={currentNote}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
               deleteNote={deleteNote}
@@ -92,7 +71,7 @@ export default function App() {
               currentNoteId &&
               notes.length > 0 &&
               <Editor
-                currentNote={findCurrentNote()}
+                currentNote={currentNote}
                 updateNote={updateNote}
               />
             }
