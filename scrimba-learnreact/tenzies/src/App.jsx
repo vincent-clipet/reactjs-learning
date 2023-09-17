c4f627e1b942588abb2792faddde7efeddce4793
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-17 14:32:56 +0200

    Tenzies: added die component & rendering using flexbox

--- scrimba-learnreact/tenzies/src/App.jsx
+++ scrimba-learnreact/tenzies/src/App.jsx
@@ -1,13 +1,49 @@
 import { useState } from 'react'
+import Die from './components/Die'
 
 
 
 function App() {
 
+  function getRandomDieValue() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  const dies = [
+    { id: 1, value: getRandomDieValue() },
+    { id: 2, value: getRandomDieValue() },
+    { id: 3, value: getRandomDieValue() },
+    { id: 4, value: getRandomDieValue() },
+    { id: 5, value: getRandomDieValue() },
+    { id: 6, value: getRandomDieValue() },
+    { id: 7, value: getRandomDieValue() },
+    { id: 8, value: getRandomDieValue() },
+    { id: 9, value: getRandomDieValue() },
+    { id: 10, value: getRandomDieValue() }
+  ]
+
+
+
+
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <main>
+      <div className='board'>
+        <div className='game'>
+          <section className='dies'>
+            {
+              dies.map((die) =>
+                <Die
+                  key={die.id}
+                  id={die.id}
+                  value={die.value}
+                />
+              )
+            }
+          </section>
+        </div>
+      </div>
+    </main>
   )
 }
 
---
+++ scrimba-learnreact/tenzies/src/components/Die.jsx
@@ -0,0 +1,8 @@
+export default function Die({ id, value }) {
+	console.log("jey")
+	return (
+		<div className="die">
+			<span className="die-value">{value}</span>
+		</div>
+	)
+}
